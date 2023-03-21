f5467ac730689d9226262b9ac0053a7f632ab0f7
Fabian Schlieper <fabian@schlieper.email>
2023-03-21 14:15:22 +0100

    protection

--- 3_Device_Protection.ino
+++ 3_Device_Protection.ino
@@ -16,21 +16,22 @@ void Device_Protection(){
     else{}                                                                                          // TO ADD: sleep and charging pause if too many errors persists   
   } 
   //FAULT DETECTION     
-  ERR = 0;                                                                                          //Reset local error counter
-  backflowControl();                                                                                //Run backflow current protection protocol  
-  if(temperature>temperatureMax)                           {OTE=1;ERR++;errorCount++;}else{OTE=0;}  //OTE - OVERTEMPERATURE: System overheat detected
-  if(currentInput>currentInAbsolute)                       {IOC=1;ERR++;errorCount++;}else{IOC=0;}  //IOC - INPUT  OVERCURRENT: Input current has reached absolute limit
-  if(currentOutput>currentOutAbsolute)                     {OOC=1;ERR++;errorCount++;}else{OOC=0;}  //OOC - OUTPUT OVERCURRENT: Output current has reached absolute limit 
-  if(voltageOutput>voltageBatteryMax+voltageBatteryThresh) {OOV=1;ERR++;errorCount++;}else{OOV=0;}  //OOV - OUTPUT OVERVOLTAGE: Output voltage has reached absolute limit                     
-  if(voltageInput<vInSystemMin&&voltageOutput<vInSystemMin){FLV=1;ERR++;errorCount++;}else{FLV=0;}  //FLV - Fatally low system voltage (unable to resume operation)
+  ERR = 0;                                                                                          //Reset local error counter                                                                             //Run backflow current protection protocol  
+  if(temperature>temperatureMax)                           {OTE=1;ERR++;errorCount++; Serial.println("!!! OVERTEMP!!!"); }else{OTE=0;}  //OTE - OVERTEMPERATURE: System overheat detected
+  if(currentInput>currentInAbsolute)                       {IOC=1;ERR++;errorCount++;Serial.println("!!! IOC (hardcoded) !!!"); }else{IOC=0;}  //IOC - INPUT  OVERCURRENT: Input current has reached absolute limit
+  if(currentOutput>currentOutAbsolute)                     {OOC=1;ERR++;errorCount++;Serial.println("!!! OOC (hardcoded) !!!"); }else{OOC=0;}  //OOC - OUTPUT OVERCURRENT: Output current has reached absolute limit 
+  if(voltageOutput>voltageBatteryMax+voltageBatteryThresh) {OOV=1;ERR++;errorCount++;Serial.println("!!! OOV (usr) !!!"); }else{OOV=0;}  //OOV - OUTPUT OVERVOLTAGE: Output voltage has reached absolute limit                     
+  if(voltageInput<vInSystemMin&&voltageOutput<vInSystemMin){FLV=1;ERR++;errorCount++;Serial.println("!!! FLV !!!"); }else{FLV=0;}  //FLV - Fatally low system voltage (unable to resume operation)
 
-  if(output_Mode==0){                                                                               //PSU MODE specific protection protocol
+  if(output_Mode==0){   
+    // PSU mode                                                                            //PSU MODE specific protection protocol
     REC = 0; BNC = 0;                                                                               //Clear recovery and battery not connected boolean identifiers
-    if(voltageInput<voltageBatteryMax+voltageDropout){IUV=1;ERR++;errorCount++;}else{IUV=0;}        //IUV - INPUT UNDERVOLTAGE: Input voltage is below battery voltage (for psu mode only)                     
+    if(voltageInput<voltageBatteryMax+voltageDropout){IUV=1;ERR++;errorCount++; Serial.println("!!! PSU IUV !!!"); }else{IUV=0;}        //IUV - INPUT UNDERVOLTAGE: Input voltage is below battery voltage (for psu mode only)                     
   }
-  else{                                                                                             //Charger MODE specific protection protocol
-    backflowControl();                                                                              //Enable backflow current detection & control                           
-    if(voltageOutput<vInSystemMin)                   {BNC=1;ERR++;}      else{BNC=0;}               //BNC - BATTERY NOT CONNECTED (for charger mode only, does not treat BNC as error when not under MPPT mode)
-    if(voltageInput<voltageBatteryMax+voltageDropout){IUV=1;ERR++;REC=1;}else{IUV=0;}               //IUV - INPUT UNDERVOLTAGE: Input voltage is below max battery charging voltage (for charger mode only)     
+  else{ 
+    // charger mode                                                                                            //Charger MODE specific protection protocol                
+    if(voltageOutput<vInSystemMin)                   {BNC=1;ERR++; Serial.println("!!! Battery not connected !!!");}      else{BNC=0;}               //BNC - BATTERY NOT CONNECTED (for charger mode only, does not treat BNC as error when not under MPPT mode)
+    if(voltageInput<voltageBatteryMax+voltageDropout){IUV=1;ERR++;REC=1;  Serial.println("!!! IUV (usr) !!!");}else{IUV=0;}               //IUV - INPUT UNDERVOLTAGE: Input voltage is below max battery charging voltage (for charger mode only)     
   } 
+  backflowControl(); //Enable backflow current detection & control           
 }
