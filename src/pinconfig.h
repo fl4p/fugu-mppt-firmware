fd1157486133289679999673be4e912c3a272620
Fabian Schlieper <fabian@schlieper.email>
2023-06-15 18:49:45 +0200

    code format

--- src/pinconfig.h
+++ src/pinconfig.h
@@ -3,7 +3,7 @@
 #include <cstdint>
 #include <Esp.h>
 
-enum class PinConfigESP32S3  : uint8_t {
+enum class PinConfigESP32S3 : uint8_t {
     // https://www.espressif.com/sites/default/files/documentation/esp32-s3-wroom-1_wroom-1u_datasheet_en.pdf#page=10
     I2C_SDA = 42,
     I2C_SCL = 2,
@@ -18,10 +18,10 @@ enum class PinConfigESP32S3  : uint8_t {
     Fan = 36,
 
 
-    buttonLeft = 39    ,
-    buttonRight = 37   ,
-    buttonBack = 40   ,
-    buttonSelect = 1 ,
+    buttonLeft = 39,
+    buttonRight = 37,
+    buttonBack = 40,
+    buttonSelect = 1,
 
     ADC_Vin = ADC1_CHANNEL_4,
     ADC_Vout = ADC1_CHANNEL_5,
@@ -29,7 +29,7 @@ enum class PinConfigESP32S3  : uint8_t {
 
 };
 
-enum class PinConfigESP32  : uint8_t {
+enum class PinConfigESP32 : uint8_t {
     I2C_SDA = 21,
     I2C_SCL = 22,
     ADC_ALERT = 34,
@@ -43,10 +43,10 @@ enum class PinConfigESP32  : uint8_t {
     Fan = 16,
 
 
-    buttonLeft = 18    ,
-    buttonRight = 17   ,
-    buttonBack =19   ,
-    buttonSelect =23 ,
+    buttonLeft = 18,
+    buttonRight = 17,
+    buttonBack = 19,
+    buttonSelect = 23,
 
     ADC_Vin = ADC1_CHANNEL_3,
     ADC_Vout = ADC1_CHANNEL_6,
@@ -54,8 +54,7 @@ enum class PinConfigESP32  : uint8_t {
 };
 
 
-const char * getChipId();
-
+const char *getChipId();
 
 
 #if CONFIG_IDF_TARGET_ESP32S3
@@ -65,10 +64,10 @@ typedef PinConfigESP32 PinConfig;
 #endif
 
 uint8_t getBuckIN_PIN() {
-    if(ESP.getEfuseMac() == 0x704082188534 /* white dot on cover near pin 16*/) {
+    if (ESP.getEfuseMac() == 0x704082188534 /* white dot on cover near pin 16*/) {
         ESP_LOGI("pins", "Using pin 17 for Bridge_IN");
         return 17;
     }
-    return (uint8_t)PinConfig::Bridge_IN;
+    return (uint8_t) PinConfig::Bridge_IN;
 }
 
