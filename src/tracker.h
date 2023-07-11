918f6987cb761e95b008c64ef5cd20ef496100ff
Fabian Schlieper <fabian@schlieper.email>
2023-07-11 22:23:04 +0200

    reset avgPower

--- src/tracker.h
+++ src/tracker.h
@@ -9,53 +9,18 @@ struct MPP {
     //float voltage = 0;
 };
 
-template<typename T, typename I>
-class AdaptiveEWMATable {
-    static constexpr auto stdSpan = 200;
-
-    std::array<float, 2048> avg{};
-    std::array<unsigned long, 2048> time{};
-
-    float alpha;
-
-    PctChange<float> pctPower{};
-    EWMA<float> stdPower{stdSpan};
-
-    unsigned int num = 0;
-
-    void add(T x, I index) {
-
-        if unlikely(isnan(x)) return;
-
-        auto now = millis();
-
-        stdPower.add(pctPower.next(x));
-
-        if (num % stdSpan == 0) {
-            // central limit theorem uncertainty to update ewm span
-            float ns = stdPower.get() / 0.005;
-            auto newSpan = constrain(ns * ns, 40, 400);
-            alpha = (2.f / (newSpan + 1.f));
-            ESP_LOGI("track", "Power std=%.6f, newSpan=%.1f, alpha=%.3f", stdPower.get(), newSpan, alpha);
-        }
-
-        auto &y(avg[index]);
-
-        if (now - time[index] > 1000 * 60) y = NAN; // invalidate old
-        if unlikely(isnan(y)) y = x; // reset
-        y = (1 - alpha) * y + alpha * x; // ewm update
-        time[index] = now;
 
 
-        ++num;
-    }
-};
-
+/**
+ * Implement local MPP tracking perturb / observe.
+ * If it doesn't find a new MPP for some time, it'll switch into slow mode.
+ * Slow mode uses longer smoothing filters, a lower update frequency and a slower perturbation speed
+ */
 struct Tracker {
     float minPowerStep = 1.5f;
     float frequency = 20;
 
-    bool _direction = false;
+    bool _direction = false; //true => increase duty cycle/decrease solar voltage
     unsigned long _time = 0;
     unsigned long _timeLastReverse = 0;
 
@@ -67,10 +32,6 @@ struct Tracker {
     MeanAccumulator _powerBuf{};
 
     //RunningMedian3<float> med3{};
-
-
-
-
     //bool _sweeping = false;
     //unsigned long _lastSweepTime = 0;
 
@@ -156,10 +117,11 @@ struct Tracker {
         if (maxPowerPoint.power > 1 && now - maxPowerPoint.timestamp > 1000 * 15) {
             speed = .02;
             frequency = 1;
-            minPowerStep = 1.5;
-            if (!prevSlow) {
+            minPowerStep = 1.5f; // 0.75 is too small
+            if (!slowMode) {
                 ESP_LOGI("mppt", "Near MPP, slow-down, set dutyCycle %hu", maxPowerPoint.dutyCycle);
-                prevSlow = true;
+                slowMode = true;
+                avgPower.reset();
                 // out power measurement with hall sensor is poor and non-linear, so in slow-mode we might stay in a
                 // local maximum. this work-around captures MPP duty cycle during normal mode
                 return (float) maxPowerPoint.dutyCycle - (float) dutyCycle;
@@ -175,3 +137,49 @@ struct Tracker {
         return _direction ? speed : -speed;
     }
 };
+
+/**
+
+template<typename T, typename I>
+class AdaptiveEWMATable {
+    static constexpr auto stdSpan = 200;
+
+    std::array<float, 2048> avg{};
+    std::array<unsigned long, 2048> time{};
+
+    float alpha;
+
+    PctChange<float> pctPower{};
+    EWMA<float> stdPower{stdSpan};
+
+    unsigned int num = 0;
+
+    void add(T x, I index) {
+
+        if unlikely(isnan(x)) return;
+
+        auto now = millis();
+
+        stdPower.add(pctPower.next(x));
+
+        if (num % stdSpan == 0) {
+            // central limit theorem uncertainty to update ewm span
+            float ns = stdPower.get() / 0.005;
+            auto newSpan = constrain(ns * ns, 40, 400);
+            alpha = (2.f / (newSpan + 1.f));
+            ESP_LOGI("track", "Power std=%.6f, newSpan=%.1f, alpha=%.3f", stdPower.get(), newSpan, alpha);
+        }
+
+        auto &y(avg[index]);
+
+        if (now - time[index] > 1000 * 60) y = NAN; // invalidate old
+        if unlikely(isnan(y)) y = x; // reset
+        y = (1 - alpha) * y + alpha * x; // ewm update
+        time[index] = now;
+
+
+        ++num;
+    }
+};
+
+*/
