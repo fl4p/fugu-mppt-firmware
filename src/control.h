5386d415fd2dbcf0aaf117ea6e02f15d2b019c98
Fabian Schlieper <fabian@schlieper.email>
2023-07-28 21:55:25 +0200

    add PD_Control_SmoothTarget

--- src/control.h
+++ src/control.h
@@ -3,7 +3,7 @@
 /**
  * PD controller (PID without I component)
  */
-struct PD {
+struct PD_Control {
     const float P, D;
     const bool normalize;
     float _prevE = NAN;
@@ -14,7 +14,7 @@ struct PD {
      * @param D derivative gain
      * @param normalize normalize input values. Compute relative error.
      */
-    PD(float P, float D, bool normalize) : P(P), D(D), normalize(normalize) {}
+    PD_Control(float P, float D, bool normalize) : P(P), D(D), normalize(normalize) {}
 
     /**
      * Clears memory. Next D component will be 0
@@ -23,7 +23,7 @@ struct PD {
         _prevE = NAN;
     }
 
-    float update(float actual, float target) {
+    virtual float update(float actual, float target) {
         if (normalize) {
             actual /= target;
             target = 1;
@@ -33,5 +33,14 @@ struct PD {
         _prevE = e;
         if (std::isnan(de)) de = 0; // first D component is 0
         return P * e + D * de;
+    }
+};
+
+struct PD_Control_SmoothTarget : PD_Control {
+    EWMA<float> _ewma;
+    PD_Control_SmoothTarget(float P, float D,  int smoothSpan) : PD_Control(P,D,true), _ewma(smoothSpan) {}
+    float update(float actual, float target) override {
+        _ewma.add(target);
+        return PD_Control::update(actual, _ewma.get());
     }
 };
