51bf806d28a5e0be0181a0169b4d4d5f80c3b600
Fabian Schlieper <fabian@schlieper.email>
2023-10-07 21:25:14 +0100

    ConfFile

--- src/store.h
+++ src/store.h
@@ -265,9 +265,9 @@ trim(const std::string &s) { // removes whitespace characters from beginnig and 
 class ConfFile {
 
     std::unordered_map<std::string, std::string> _map;
-
+    const char *path;
 public:
-    explicit ConfFile(const char *path) {
+    explicit ConfFile(const char *path) : path(path) {
         std::ifstream file(path);
         if (file.is_open()) {
             std::string line;
@@ -283,27 +283,84 @@ public:
                     ESP_LOGE(TAG, "error reading %s: '=' not found in line '%s'", path, line.c_str());
                     assert(false);
                 }
-                _map[trim(line.substr(0, ie))] = trim(line.substr(ie + 1));
+                auto k = trim(line.substr(0, ie));
+                if (_map.find(k) != _map.end()) {
+                    ESP_LOGE(TAG, "duplicate key %s in file '%s'", k.c_str(), path);
+                    assert(false);
+                }
+                _map[k] = trim(line.substr(ie + 1));
             }
             file.close();
         }
     }
 
+
+    void add(const std::unordered_map<std::string, std::string> &values) {
+        FILE *f = fopen(path, "a");
+        if (f == nullptr) {
+            f = fopen(path, "w");
+            if (f == nullptr) {
+                ESP_LOGE("store", "Cannot write %s", path);
+            }
+            assert (f != nullptr);
+        }
+
+
+        for (auto [key, val]: values) {
+            if (_map.find(key) != _map.end()) {
+                ESP_LOGE(TAG, "cannot add duplicate key %s", key.c_str());
+                //assert(false);
+                throw std::runtime_error("duplicate key: " + key);
+            }
+            fputc('\n', f);
+            assert(fwrite(key.c_str(), key.length(), 1, f) == 1);
+            assert(fwrite(" = ", 3, 1, f) == 1);
+            assert(fwrite(val.c_str(), val.length(), 1, f) == 1);
+        }
+
+#ifndef CONFIG_LITTLEFS_FLUSH_FILE_EVERY_WRITE
+        if (fsync(fileno(f)) != 0) {
+            fclose(f);
+            assert(false);
+        }
+#endif
+
+        fclose(f);
+
+
+/*
+        std::ofstream file(path, std::ios_base::app);
+        if(!file.is_open()) file = std::ofstream (path, )
+        assert(file.is_open());
+        for (auto [key, val]: values) {
+            if (_map.find(key) != _map.end()) {
+                ESP_LOGE(TAG, "cannot add duplicate key %s", key.c_str());
+                assert(false);
+            }
+            file << std::endl << key << " = " << val;
+        }
+         */
+    }
+
     template<typename T>
     // # (const char *, char **)
-    T getX(const std::string &key, T def, const std::function<T(const char *, char **)> &strto_, bool noDef=false) const {
+    T getX(const std::string &key, T def, const std::function<T(const char *, char **)> &strto_,
+           bool noDef = false) const {
         // strto_ error handling https://stackoverflow.com/questions/26080829/detecting-strtol-failure
         auto i = _map.find(key);
         if (i != _map.end()) {
             char *endptr = nullptr;
+            errno = 0; // reset
             T l = strto_(i->second.c_str(), &endptr);
             if (errno != 0) {
-                ESP_LOGE(TAG, "strtol(%s) failed: ret=%f, errno=%i", i->second.c_str(), (float) l, errno);
-                assert(false);
+                ESP_LOGE(TAG, "%s:%s: strto_(\"%s\") failed: ret=%f, errno=%i", path, key.c_str(), i->second.c_str(),
+                         (float) l, errno);
+                throw std::runtime_error("strto_ error " + i->second);
             }
             if (*endptr != 0) {
                 ESP_LOGE(TAG, "additional chars after strtol(%s): '%s'", i->second.c_str(), endptr);
-                assert(false);
+                //assert(false);
+                throw std::runtime_error("additional chars " + i->second);
             }
             return l;
         }
@@ -312,14 +369,15 @@ public:
             auto v = keys();
             std::string s = std::accumulate(v.begin(), v.end(), std::string{});
             ESP_LOGE(TAG, "key '%s' not found in %s", key.c_str(), s.c_str());
-            assert(false);
+            //assert(false);
+            throw std::runtime_error("key not found: " + key);
         }
 
         return def;
     }
 
     std::vector<std::string> keys() const {
-        std::vector<std::string> keys;
+        std::vector<std::string> keys{_map.size()};
         std::transform(_map.begin(), _map.end(), keys.begin(),
                        [](const std::pair<std::string, std::string> &p) { return p.first; });
         return keys;
@@ -348,7 +406,7 @@ public:
         return getX<float>(key, def, std::strtof);
     }
 
-    float f(const std::string &key) { return getFloat(key); }
+    float f(const std::string &key, float def = std::numeric_limits<float>::max()) { return getFloat(key, def); }
 
     const std::string &getString(const std::string &key, const std::string &def = "") {
         //static std::string empty;
@@ -369,6 +427,8 @@ public:
         }
         return def;
     }
+
+    explicit operator bool() const { return !_map.empty(); }
 };
 
 #undef TAG
