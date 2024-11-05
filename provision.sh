f0b4f5acfc8781ad213c1e2cf9d239f485cb91ab
Fabian <fabian@schlieper.email>
2024-11-05 14:41:31 +0000

    add fugu_int_adc provisioning

---
+++ provision.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+BOARD="$1"
+echo "BOARD=$BOARD"
+if [[ ! -d "provisioning/$BOARD" ]] || [[ "" == "$BOARD" ]]; then
+  echo "invalid board, choose from"
+  ls provisioning/
+  exit 1
+fi
+
+# TODO
+# check pins.conf/mcu == current target chip
+
+littlefs-python create provisioning/"$BOARD" "$BOARD".bin -v --fs-size=0x20000 --name-max=64 --block-size=4096
+parttool.py --port /dev/cu.usb* write_partition --partition-name littlefs --input "$BOARD".bin
