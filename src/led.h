da1f3c37a78ecc0ac7fa5ffeabd49f9fd833a308
Fabian <fabian@schlieper.email>
2024-12-04 10:12:38 +0000

    led use led_strip component

--- src/led.h
+++ src/led.h
@@ -1,7 +1,13 @@
+#if USE_FASTLED
 #include <FastLED.h>
+#else
+
+#include "led_strip.h"
+
+#endif
 
 #include "pinconfig.h"
-
+#include "store.h"
 
 /**
  * topping mode: blue 0,9,48
@@ -9,19 +15,57 @@
 
 
 class LedIndicator {
+#if USE_FASTLED
     CRGB leds[1]{0};
+#else
+    led_strip_handle_t led_strip;
+#endif
 
     bool enabled = false;
 
 public:
     bool begin(const ConfFile &pinConfig) {
-        if (!pinConfig.getByte("led_WS2812B", 0))
-            return false;
-        assert(pinConfig.getByte("led_WS2812B") == 1);
+        std::string k = "led_WS2812";
+        auto pin = pinConfig.getByte(k, 0);
+        if (!pin) pin = pinConfig.getLong(k + "B", 0);
+        if (!pin)return false;
+
+        ESP_LOGI("led", "pin = %d", pin);
+        for (auto &s: pinConfig.keys())
+            ESP_LOGI("led", "k=%s", s.c_str());
+
+        /// LED strip common configuration
+        led_strip_config_t strip_config = {
+                .strip_gpio_num = pin,  // The GPIO that connected to the LED strip's data line
+                .max_leds = 1,                 // The number of LEDs in the strip,
+                .led_model = LED_MODEL_WS2812, // LED strip model, it determines the bit timing
+                .color_component_format = LED_STRIP_COLOR_COMPONENT_FMT_GRB, // The color component format is G-R-B
+                .flags = {
+                        .invert_out = false, // don't invert the output signal
+                }
+        };
+
+/// RMT backend specific configuration
+        led_strip_rmt_config_t rmt_config = {
+                .clk_src = RMT_CLK_SRC_DEFAULT,    // different clock source can lead to different power consumption
+                .resolution_hz = 10 * 1000 * 1000, // RMT counter clock frequency: 10MHz
+                .mem_block_symbols = 64,           // the memory size of each RMT channel, in words (4 bytes)
+                .flags = {
+                        .with_dma = false, // DMA feature is available on chips like ESP32-S3/P4
+                }
+        };
+
+        /// Create the LED strip object
+        ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
+
+#if USE_FASTLED
+        assert(pin == 1);
         constexpr uint8_t LED_PIN = 1;
         FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, 1);
         FastLED.setBrightness(15);
-        setHexShort(0x210);
+#endif
+
+        setHexShort(0x111);
         enabled = true;
         return true;
     }
@@ -29,13 +73,23 @@ public:
     void setRGB(uint8_t ir, uint8_t ig, uint8_t ib) {
         if (!enabled)return;
 
+#if USE_FASTLED
         auto rgb = CRGB(ir, ig, ib);
         if (rgb != leds[0])
             ESP_LOGD("LED", "setRgb(%hhu, %hhu, %hhu)", ir, ig, ib);
         //else return;
         leds[0] = rgb;
         FastLED.show();
-
+#else
+        if (!ir && !ig && !ib) {
+            ESP_LOGI("led", "Clear!");
+            ESP_ERROR_CHECK(led_strip_clear(led_strip));
+        } else {
+            ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, 0, ir, ig, ib));
+            ESP_ERROR_CHECK(led_strip_refresh(led_strip));
+            ESP_LOGI("led", "R=%d,G=%d,B=%d", ir, ig, ib);
+        }
+#endif
     }
 
     void setRGB(const char *hex) {
