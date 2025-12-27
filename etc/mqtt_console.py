cebc5f234e445d3f49ba5ba731c084987fc345a0
Fabian <fabian@schlieper.email>
2025-12-27 14:34:32 +0000

    add mqtt_console client

--- etc/mqtt_console.py
+++ etc/mqtt_console.py
@@ -1,29 +1,45 @@
 #!/usr/bin/python3
-
+import sys
 
 import paho.mqtt.client as mqtt
 from paho.mqtt.enums import CallbackAPIVersion
 
 
 def on_connect(mqttc, obj, flags, reason_code, properties):
-    print("Connected, reason_code: " + str(reason_code))
+    logger.info("Connected, reason_code: " + str(reason_code))
 
 
 def on_message(mqttc, obj, msg):
     name = msg.topic.split('/')[-1]
     line = msg.payload.decode('utf-8').strip()
     if line:
-        print('%s: %s' % (name, line))
-    #print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+        # print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload), flush=True)
+        logger.info('%8s: %s', name, line)
 
 
 def on_subscribe(mqttc, obj, mid, reason_code_list, properties):
-    print("Subscribed: " + str(mid) + " " + str(reason_code_list))
+    logger.info("Subscribed: " + str(mid) + " " + str(reason_code_list))
 
 
 def on_log(mqttc, obj, level, string):
-    print('log:', string)
+    logger.info('log:', string)
 
+
+import logging
+from logging.handlers import RotatingFileHandler
+
+FORMAT = '%(asctime)s %(message)s'  # https://docs.python.org/3/library/logging.html#logging.Logger.debug
+fmt = logging.Formatter(FORMAT)
+# fmt.default_time_format = '%m-%d %H:%M:%S'
+
+logger = logging.getLogger('console')
+logger.setLevel(logging.INFO)
+handler = RotatingFileHandler('fugu_console.log', maxBytes=10_000_000, backupCount=10)
+handler.setFormatter(fmt)
+logger.addHandler(handler)
+std_h = logging.StreamHandler(stream=sys.stdout)
+std_h.setFormatter(fmt)
+logger.addHandler(std_h)
 
 # If you want to use a specific client id, use
 # mqttc = mqtt.Client("client-id")
@@ -35,9 +51,14 @@ mqttc.on_connect = on_connect
 mqttc.on_subscribe = on_subscribe
 # Uncomment to enable debug messages
 # mqttc.on_log = on_log
+
+topic = "pv/log/+"
+
 mqttc.username_pw_set("pv", "0ffgrid")
+logger.info('connecting broker..')
 mqttc.connect("havan.local", 1882, 60)
 # pv/log/fugu-esp32s3-100100C40A24
-mqttc.subscribe("pv/log/+")
+logger.info('connected, subscribing %r', topic)
+mqttc.subscribe(topic)
 
 mqttc.loop_forever()
